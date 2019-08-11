89671d6c88d0c418ad1bd635f62d052c26c8ffd8
Sung-HwanLee <hwan.lee67@gmail.com>
2019-08-11 21:38:58 +0900

    Revert change on Language.v

--- src/Language.v
+++ src/Language.v
@@ -1,12 +1,16 @@
 Set Implicit Arguments.
 
 Module Language.
-  Structure t := mk {
-    syntax: Type;
-    state: Type;
+  Section Language.
+    Variable E: Type.
 
-    init: syntax -> state;
-    is_terminal: state -> Prop;
-    step: forall {E: Type} (e:E) (s1:state) (s2:state), Prop;
-  }.
+    Structure t := mk {
+      syntax: Type;
+      state: Type;
+
+      init: syntax -> state;
+      is_terminal: state -> Prop;
+      step: forall (e:E) (s1:state) (s2:state), Prop;
+    }.
+  End Language.
 End Language.
